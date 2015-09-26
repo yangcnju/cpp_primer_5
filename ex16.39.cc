9acaea69ed5a37eff3bf0e5c7307628d586044fe
yangcnju <yangcnju@gmail.com>
2015-09-25 21:10:26 -0400

    ch16.2.2 expicit arg

---
+++ ex16.39.cc
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+int compare(const T &v1, const T &v2)
+{
+	if (v1 < v2) return -1;
+	if (v2 < v1) return 1;
+	return 0;
+}
+
+int main()
+{
+	cout << compare<string>("str1", "str2") << endl;
+	return 0;
+}
